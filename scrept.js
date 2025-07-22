503ecd00b12b191d00911d34c77a06de0dd617e2
AKASH DHANWATE <learnaids2025@gmail.com>
2025-07-22 11:14:24 +0530

    Add files via upload

---
+++ scrept.js
@@ -0,0 +1,4 @@
+// JavaScript to show a console message and future interactions
+console.log("Welcome to Akash Dhanwate’s portfolio site!");
+
+// You can add interactivity here later (e.g. button animations, form handling, etc.)
